5ff507ccd79ca0c6af4f3be12538cac63392c3f8
cominggithob <misscoming@gmail.com>
2013-06-22 22:57:17 +0800

    Add debug menu

---
+++ NaviUtil/SystemConfig.h
@@ -0,0 +1,13 @@
+//
+//  SystemConfig.h
+//  NaviUtil
+//
+//  Created by Coming on 13/6/19.
+//  Copyright (c) 2013年 Coming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SystemConfig : NSObject
+
+@end
---
+++ NaviUtil/SystemConfig.m
@@ -0,0 +1,13 @@
+//
+//  SystemConfig.m
+//  NaviUtil
+//
+//  Created by Coming on 13/6/19.
+//  Copyright (c) 2013年 Coming. All rights reserved.
+//
+
+#import "SystemConfig.h"
+
+@implementation SystemConfig
+
+@end
